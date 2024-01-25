54a1f05049a7a1ce5f0f623aecdd0355adda41da
chy <chlghksdyd31@gmail.com>
2024-01-25 22:19:41 +0900

    2024-01-25-2

--- spring_MVC_2/upload/upload/src/main/java/hello/upload/file/FileStore.java
+++ spring_MVC_2/upload/upload/src/main/java/hello/upload/file/FileStore.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 public class FileStore {
     @Value("${file.dir}")
     private String fileDir;
-    public String extractExt(String filename){
+    public String extractExt(String filename){  
         int index = filename.lastIndexOf(".");
         return filename.substring(index+1);
     }
