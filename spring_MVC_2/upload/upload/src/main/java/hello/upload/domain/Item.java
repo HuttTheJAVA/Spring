045481786410213cfb9ebc225f7c5788f06fd913
unknown <chlghksdyd31@gmail.com>
2024-01-25 17:53:36 +0900

    2024-01-25

--- spring_MVC_2/upload/upload/src/main/java/hello/upload/domain/Item.java
+++ spring_MVC_2/upload/upload/src/main/java/hello/upload/domain/Item.java
@@ -8,6 +8,6 @@ import java.util.List;
 public class Item {
     private Long id;
     private String itemName;
-    private UploadFile attachfile;
+    private UploadFile attachFile;
     private List<UploadFile> imageFiles;
 }
